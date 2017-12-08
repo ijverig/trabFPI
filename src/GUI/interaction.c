b4fa0a360cea0bc926095864ecfbbf9d30b7de3e
Daniel Schmidt <silveirinha@gmail.com>
2017-12-07 22:44:03 -0200

    added flipping commands

--- src/GUI/interaction.c
+++ src/GUI/interaction.c
@@ -17,6 +17,12 @@ void handle_selection(int option)
     switch (option) {
         case OPTION_NOTHING:
             break;
+        case OPTION_FLIP_H:
+            transform_flip_h(&session.buffer.image);
+            break;
+        case OPTION_FLIP_V:
+            transform_flip_v(&session.buffer.image);
+            break;
         case OPTION_GRAYSCALE:
             filter_grayscale(&session.buffer.image);
             break;
@@ -35,7 +41,13 @@ void create_menu()
 {
     char *const SEPARATOR = "————————";
 
+    int flip_menu = glutCreateMenu(handle_selection);
+    glutAddMenuEntry("Horizontally (h)", OPTION_FLIP_H);
+    glutAddMenuEntry("Vertically (v)", OPTION_FLIP_V);
+
     int main_menu = glutCreateMenu(handle_selection);
+    glutAddSubMenu("Flip", flip_menu);
+    glutAddMenuEntry(SEPARATOR, OPTION_NOTHING);
     glutAddMenuEntry("Grayscale (g)", OPTION_GRAYSCALE);
     glutAddMenuEntry(SEPARATOR, OPTION_NOTHING);
     glutAddMenuEntry("Exit (e)", OPTION_EXIT);
@@ -47,6 +59,12 @@ void create_menu()
 void handle_key_press(unsigned char key, int _, int __)
 {
     switch (key) {
+        case 'h':
+            handle_selection(OPTION_FLIP_H);
+            break;
+        case 'v':
+            handle_selection(OPTION_FLIP_V);
+            break;
         case 'g':
             handle_selection(OPTION_GRAYSCALE);
             break;
